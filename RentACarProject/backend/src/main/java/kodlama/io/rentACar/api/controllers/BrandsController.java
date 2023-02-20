f3c568dd0dec540b71f8a23002919de9421e522e
aliemregk <aliemregk@outlook.com>
2023-02-20 14:50:36 +0300

    Add getbyid, update and delete endpoints for brand

--- RentACarProject/backend/src/main/java/kodlama/io/rentACar/api/controllers/BrandsController.java
+++ RentACarProject/backend/src/main/java/kodlama/io/rentACar/api/controllers/BrandsController.java
@@ -2,34 +2,56 @@ package kodlama.io.rentACar.api.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlama.io.rentACar.business.abstracts.BrandService;
 import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.requests.UpdateBrandRequest;
 import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentACar.business.responses.GetByIdBrandResponse;
+import lombok.AllArgsConstructor;
+
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/brands")
+@AllArgsConstructor
 public class BrandsController {
 
     private BrandService brandService;
 
-    public BrandsController(BrandService brandService) {
-        this.brandService = brandService;
-    }
-
-    @GetMapping(value = "/getall")
+    @GetMapping()
     public List<GetAllBrandsResponse> getAll() {
         return brandService.getAll();
     }
 
-    @PostMapping(value = "/add")
-    public void addBrand(@RequestBody CreateBrandRequest brandRequest) {
-        this.brandService.add(brandRequest);
+    @GetMapping("/{id}")
+    public GetByIdBrandResponse getById(@PathVariable int id) {
+        return brandService.getById(id);
+    }
+
+    @PostMapping()
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public void add(@RequestBody CreateBrandRequest createBrandRequest) {
+        this.brandService.add(createBrandRequest);
+    }
+
+    @PutMapping()
+    public void update(@RequestBody UpdateBrandRequest updateBrandRequest) {
+        this.brandService.update(updateBrandRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id) {
+        this.brandService.delete(id);
     }
 
 }
--- RentACarProject/backend/src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
+++ RentACarProject/backend/src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
@@ -1,15 +1,15 @@
 package kodlama.io.rentACar.business.concretes;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
 import kodlama.io.rentACar.business.abstracts.BrandService;
 import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.requests.UpdateBrandRequest;
 import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentACar.business.responses.GetByIdBrandResponse;
 import kodlama.io.rentACar.core.utilities.mappers.ModelMapperService;
 import kodlama.io.rentACar.entities.concretes.Brand;
 import lombok.AllArgsConstructor;
@@ -31,9 +31,26 @@ public class BrandManager implements BrandService {
     }
 
     @Override
+    public GetByIdBrandResponse getById(int id) {
+        Brand brand = brandRepository.findById(id).orElseThrow();
+        return mapperService.forResponse().map(brand, GetByIdBrandResponse.class);
+    }
+
+    @Override
     public void add(CreateBrandRequest createBrandRequest) {
         Brand brandToAdd = mapperService.forRequest().map(createBrandRequest, Brand.class);
         this.brandRepository.save(brandToAdd);
+    }
+
+    @Override
+    public void update(UpdateBrandRequest updateBrandRequest) {
+        Brand brandToAdd = mapperService.forRequest().map(updateBrandRequest, Brand.class);
+        this.brandRepository.save(brandToAdd);
+    }
+
+    @Override
+    public void delete(int id) {
+        this.brandRepository.deleteById(id);
     }
 
 }
