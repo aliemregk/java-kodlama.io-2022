be6100f4ca2fadb5650d5604d9862f73a7e484e9
aliemregk <aliemregk@outlook.com>
2023-03-06 17:24:57 +0300

    Add validation and business rule handler

---
+++ Devs/src/main/java/Kodlama/io/Devs/core/utils/exceptions/ProblemDetails.java
@@ -0,0 +1,12 @@
+package Kodlama.io.Devs.core.utils.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProblemDetails {
+    private String message;
+}
